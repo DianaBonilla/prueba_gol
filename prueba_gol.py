7a2e76c1fde68f4e1bec50fce561130cb3550bac
USUARIO05-PC\Usuario01 <dianabonilla2512@gmail.com>
2017-01-04 13:13:00 -0500

    salida

--- prueba_gol.py
+++ prueba_gol.py
@@ -8,7 +8,6 @@ posXbalon = 950 # posicion inicial del balon en x
 posYbalon = 150 # posicion inicial del balon en y
 posXarco = 100  # posicion del arco en x
 posYarco = 100  # posicion del arco y
-posXbalon1 = posXbalon
 movimiento = 30 ## "velocidad a la que se mueve"
 Blanco = (253,254,254) ## color de fondo
 ##derecha=True
@@ -20,12 +19,13 @@ while True:
           if event.type == pygame.KEYDOWN: ## movimientos con las flechas
               if event.key == K_LEFT: ## movimiento a la izquierda
                   posXbalon -= movimiento
-                  posXbalon1 = posXbalon
-                  print(posXbalon1 + posYbalon)
+                  print(posXbalon + posYbalon)
               elif event.key == K_RIGHT:  ## movimiento a la derecha
                   posXbalon += movimiento
-                  posXbalon1 = posXbalon
-                  print(posXbalon1,posYbalon) 
-          if posXbalon1 and posYbalon == posXarco and posYarco:
-              print("gol")
-          pygame.display.update()
+                  print(posXbalon,posYbalon) 
+    if posXbalon < 400:
+        print("gol")
+        exit()
+    else:
+        posXbalon
+    pygame.display.update()
